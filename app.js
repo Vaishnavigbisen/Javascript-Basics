638fef1ff87fbe1b2384e9c822ac3efc080ec0d2
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2021-09-02 19:28:56 +0530

    console

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hey this is my first piece of JS code!!! YAY')
+
+console.log(123456678999)
+
+console.log({ name: "Vaishnavi" })
+
+console.log(true)
+console.log(false)
+
+console.log(false, 234243, 234243, 'string', { name: "Vaishnavi" })
+
+console.warn("This is warning")
+
+console.error("This is an error")
