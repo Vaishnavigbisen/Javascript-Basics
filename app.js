4cdee09ead3d79e8cc7548f119eb65c94e46a413
Vaishnavib1 <bisenvaishnavi2021@gmail.com>
2021-09-04 20:00:11 +0530

    intro to event listeners

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-const element = document.querySelector('#selectme')
-// element.innerHTML = "This is from JS"
-
-// element.style.color = "cyan"
-// element.style.fontSize = "40px"
-// element.style = 'display:none'
-
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
-
-// const body = document.querySelector('body')
-
-// console.log(body.classList.toggle('dark'))
-// toggle internally implements if...else to check
-// whenever toggles add the class name them it returns true, if it finds the dark already present then it would not  add the dark so it will return false
+const button = document.querySelector('button')
+const element = document.querySelector('body')
 
 
-const value = element.classList.contains('three')
 
-console.log(value)
+button.addEventListener('click', () => {
+    // console.log('you clicked me')
+    // element.style = "background:aqua"
+    element.classList.toggle('dark')
+})
