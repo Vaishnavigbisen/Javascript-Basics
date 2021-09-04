3d7ba15c620b4140c3b894c4d7c8850ca0307515
Vaishnavib1 <bisenvaishnavi2021@gmail.com>
2021-09-04 19:07:19 +0530

    DOM selection methods

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 
 // const element = document.getElementsByClassName('heading')
 
-// const element = document.getElementsByTagName('h2')
+// const element = document.getElementsByTagName('div')
 
-// const element = document.querySelector('#selectme')
+// const element = document.querySelector('.heading')
+// Query selector returns just the FIRST MATCH
+
+// const element = document.querySelectorAll('.heading')
+// QUERY SELECTOR ALL returns NODE LIST (list of nodes)- something like array
+
+const element = document.querySelector('h3.heading')
+
 console.log(element)
