8129d6d7083f8bf03a240870f00f75a9e1db6a89
Vaishnavib1 <bisenvaishnavi2021@gmail.com>
2021-09-06 19:30:23 +0530

    switch case

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-// const rock = document.querySelector('#rock')
-// const paper = document.querySelector('#paper')
-// const scissor = document.querySelector('#scissor')
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector("#board")
 
 
-function logName(Name = "Neha", age = 18) {
-    // console.log("Neha")
-    console.log(Name + " You are " + age)
+const handler = (e) => {
+    const action = e.target.id
+    // if (action === 'rock') {
+    //     board.innerHTML = "Rock crushes scissors"
+    // } else if (action === 'paper') {
+    //     board.innerHTML = "Paper traps Rock"
+    // } else if (action === 'scissor') {
+    //     board.innerHTML = "Scissor cuts paper"
+    // } else {
+    //     board.innerHTML = "Click something"
+    // }
+
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+    }
+
 }
-
-const add = (a = 0, b = 0) => {
-    return a + b
-}
+ 
 
 
-console.log(add(12, 23))
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
