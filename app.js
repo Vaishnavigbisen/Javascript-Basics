1c2b6bbf7dee3ff6ea579deedfada9d3a48dee79
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2021-09-07 18:08:12 +0530

    array methods

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// for (var i = 1; i <= 10; i++) {
-//     var age = "19"
-//     console.log("ITeration " + i)
-// }
+const balls = document.querySelectorAll('.ball')
+//const balls = document.getElementsByClasssName('ball')
+const body = document.querySelector('body')
+
+const arr = [1,2,3,4,5]
+//const added = arr.includes(3)
+//arr.shift()
+//arr.shift()
+//arr.splice()
 
 
+arr.forEach((element,iteration) => {
+    console.log(element, iteration)
+})
 
-
-let iteration = 1
-
-while (iteration < 10) {
-    console.log(iteration)
-    iteration++
-}
+// console.log(arr)
