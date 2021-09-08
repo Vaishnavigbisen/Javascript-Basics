005c7b735eaabfe739179429f531b48cae098e87
Vaishnavigbisen <vaishnavigbisen@gmail.com>
2021-09-08 20:06:36 +0530

    object destructuring

--- app.js
+++ app.js
@@ -17,3 +17,32 @@ let [x,y,z] = getscores();
 console.log(x);
 console.log(y);
 console.log(z);
+
+
+
+//Object Destructuring
+
+const obj = {
+    fullname: "Vaishnavi",
+    email: "xyz@gmail.com",
+    age: 20
+}
+
+console.log(obj.email)
+
+let {dum, age, email, fullname} = obj;
+console.log(fullname)
+console.log(dum)
+
+let person = {
+    firstname: "Nikhil",
+    lastname: "Agarwal",
+    age1: 20,
+    middlename: "DNA"
+}
+let {age1, firstname, lastname, middlename="" } = person;
+console.log(age1);
+console.log(firstname);
+console.log(lastname);
+console.log(middlename);
+
